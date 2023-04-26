4d15008c6a0e8eb5c2bd72bc4185f82165de9c6f
Michael Y Chuang <85854593+mchuangyc413@users.noreply.github.com>
2023-04-25 21:43:04 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ class App extends React.Component {
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleFocus = this.handleFocus.bind(this);
+    this.handleBlur = this.handleBlur.bind(this);
   }
 
   handleChange(event) {
@@ -28,6 +30,14 @@ class App extends React.Component {
     event.preventDefault();
   }
 
+  handleFocus(event) {
+    event.target.placeholder = '';
+  }
+
+  handleBlur(event) {
+    event.target.placeholder = event.target.name.charAt(0).toUpperCase() + event.target.name.slice(1);
+  }
+
   render() {
     return (
       <div className="App">
@@ -35,43 +45,52 @@ class App extends React.Component {
           <form onSubmit={this.handleSubmit}>
             <h2 className="form-header">Enter Your Information</h2>
             <div className="form-group">
-              <label htmlFor="name">Name</label>
+              <label htmlFor="name"></label>
               <input
                 type="text"
                 id="name"
                 name="name"
                 value={this.state.name}
                 onChange={this.handleChange}
+                onFocus={this.handleFocus}
+                onBlur={this.handleBlur}
                 required
+                placeholder="Name"
               />
               <span className="form-highlight"></span>
               <span className="form-bar"></span>
             </div>
             <div className="form-group">
-              <label htmlFor="phoneNumber">Phone Number</label>
+              <label htmlFor="phone Number"></label>
               <input
                 type="tel"
                 id="phoneNumber"
                 name="phoneNumber"
                 value={this.state.phoneNumber}
                 onChange={this.handleChange}
+                onFocus={this.handleFocus}
+                onBlur={this.handleBlur}
                 pattern="[0-9]{10}"
                 required
+                placeholder="Phone Number"
               />
               <span className="form-highlight"></span>
               <span className="form-bar"></span>
             </div>
             <div className="form-group">
-              <label htmlFor="ticketNumber">Ticket Number</label>
+              <label htmlFor="ticket Number"></label>
               <input
                 type="text"
                 id="ticketNumber"
                 name="ticketNumber"
                 value={this.state.ticketNumber}
                 onChange={this.handleChange}
+                onFocus={this.handleFocus}
+                onBlur={this.handleBlur}
                 minLength="6"
                 maxLength="10"
                 required
+                placeholder="Ticket Number"
               />
               <span className="form-highlight"></span>
               <span className="form-bar"></span>
